63d8c3e02b2018e73c73f943986758256081ba43
Marciano Moreno <marciano_moreno@outlook.com>
2022-04-14 18:43:30 -0500

    Configured application to retrieve shipping data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
-
+import { HttpClientModule } from '@angular/common/http';
+ 
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductListComponent } from './product-list/product-list.component';
@@ -14,10 +15,12 @@ import { HelloWorldInterpolationComponent } from './hello-world-interpolation/he
 import { HelloWorldNgifComponent } from './hello-world-ngif/hello-world-ngif.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
 import { CartComponent } from './cart/cart.component';
+import { ShippingComponent } from './shipping/shipping.component';
 
 @NgModule({
   imports: [
     BrowserModule,
+    HttpClientModule,
     ReactiveFormsModule,
     RouterModule.forRoot([
       { path: '', component: ProductListComponent },
@@ -41,7 +44,8 @@ import { CartComponent } from './cart/cart.component';
     HelloWorldInterpolationComponent,
     HelloWorldNgifComponent,
     ProductDetailsComponent,
-    CartComponent
+    CartComponent,
+    ShippingComponent
   ],
   bootstrap: [
     AppComponent
